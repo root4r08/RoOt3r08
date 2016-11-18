246e4caa5472fc094945283029ba3a30f2613fd6
root3r08 <root4r08@users.noreply.github.com>
2016-11-18 15:14:32 +0330

    Update rebot.lua

--- plugins/rebot.lua
+++ plugins/rebot.lua
@@ -3,7 +3,7 @@ do
 local function run(msg, matches) 
 if ( msg.text ) then
   if ( msg.to.type == "user" ) then
-     return "- Welcome to BombusMOD !\n\n- For More Information Subscribe in \n\n- Channel : @Ntflight"
+     return "- Welcome to bot08 !\n\n- For More Information Subscribe in \n\n- DEVELOPER : @root3r08"
     end 
   end 
 end 
